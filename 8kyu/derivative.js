ea3367cab1aff231e4ab1707cea194cf49c5e6e9
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-05-14 13:32:49 -0400

    added comment

--- 8kyu/derivative.js
+++ 8kyu/derivative.js
@@ -1,6 +1,6 @@
 // This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
-/Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
-
+//Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+//hello 
 function derive(coefficient,exponent) {
   
 
