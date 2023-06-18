cf6ac4cb685b8920562d2b35d353120e279049d8
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-06-18 12:19:30 -0400

    added shortest string js

---
+++ 8kyu/shortestString.js
@@ -0,0 +1,31 @@
+// Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+// The first mission: Traversing arr, find the shortest string length.
+
+// The second mission: Traversing arr again, intercept all strings to the shortest 
+// string length(Start from index0). you can use one of slice() substring() or substr()
+//  do it. return the result after finished the work.
+
+
+function cutIt(arr){
+  
+    let shortestString = arr[0].length;
+      
+      for( let i = 1; i < arr.length; i++){
+          
+         if( arr[i].length < shortestString){
+          shortestString= arr[i].length;
+           }
+        
+      }
+      
+      
+      for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].substring(0, shortestString);
+      }
+      
+      return arr;
+    }
+    
+    
+    
